ad471d9fcef21a559249ac2dc80b2487f1812159
diogoJoseFreitas <diogoJoseFreitas@proton.me>
2025-01-04 22:36:25 -0400

    Create Combobox and example made

--- src/models/ScreenModel.py
+++ src/models/ScreenModel.py
@@ -1,10 +1,10 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 from tkcalendar import DateEntry
 
 
 class ScreenModel(tk.Tk):
-
     PADX = 5
     PADY = 5
     WIDTH = 25
@@ -17,21 +17,26 @@ class ScreenModel(tk.Tk):
     def show(self):
         self.mainloop()
 
-    def label(self, text = "", positon=(0, 0), width=None, padx=PADX, pady= PADY, colspan=1, rowspan = 1, **kw):
-        tk.Label(master=self, text=text, width=width, **kw).grid(row=positon[0], column=positon[1], padx=padx, pady=pady, columnspan=colspan, rowspan=rowspan, sticky="w")
+    def label(self, text = "", positon=(0, 0), width=None, padx=PADX, pady= PADY, colspan=1, rowspan = 1, sticky="w", **kwargs):
+        tk.Label(master=self, text=text, width=width, **kwargs).grid(row=positon[0], column=positon[1], padx=padx, pady=pady, columnspan=colspan, rowspan=rowspan, sticky=sticky)
     
-    def textInput(self, positon=(0, 0), width=WIDTH, padx=PADX, pady= PADY, colspan=1, rowspan = 1, **kw) -> tk.Entry:
-        inp = tk.Entry(self, width=width, **kw)
+    def textInput(self, positon=(0, 0), width=WIDTH, padx=PADX, pady= PADY, colspan=1, rowspan = 1, **kwargs) -> tk.Entry:
+        inp = tk.Entry(self, width=width, **kwargs)
         inp.grid(row=positon[0], column=positon[1], padx=padx, pady=pady, columnspan=colspan, rowspan=rowspan, sticky="w")
         return inp
     
-    def dateInput(self, positon=(0, 0), width=10, padx=PADX, pady= PADY, colspan=1, rowspan = 1, date_pattern="dd/mm/yyyy", **kw) -> DateEntry:
+    def dateInput(self, positon=(0, 0), width=10, padx=PADX, pady= PADY, colspan=1, rowspan = 1, date_pattern="dd/mm/yyyy", **kwargs) -> DateEntry:
         inp = DateEntry(self, width=width, date_pattern=date_pattern)
         inp.grid(row=positon[0], column=positon[1], padx=padx, pady=pady, columnspan=colspan, rowspan=rowspan, sticky="w")
         return inp
     
-    def button(self, text = "", command = None, positon=(0, 0), width=WIDTH, padx=PADX, pady= PADY, colspan=1, rowspan = 1, **kw):
-        tk.Button(self, text=text, command=command, width=width, **kw).grid(row=positon[0], column=positon[1], padx=padx, pady=pady, columnspan=colspan, rowspan=rowspan, sticky="w")
+    def button(self, text = "", command = None, positon=(0, 0), width=WIDTH, padx=PADX, pady= PADY, colspan=1, rowspan = 1, **kwargs):
+        tk.Button(self, text=text, command=command, width=width, **kwargs).grid(row=positon[0], column=positon[1], padx=padx, pady=pady, columnspan=colspan, rowspan=rowspan, sticky="w")
+    
+    def combobox(self, values=[], positon=(0, 0), width=None, padx=PADX, pady= PADY, colspan=1, rowspan = 1, **kwargs):
+        inp = ttk.Combobox(self, values=values, width=width, **kwargs)
+        inp.grid(row=positon[0], column=positon[1], padx=padx, pady=pady, columnspan=colspan, rowspan=rowspan, sticky="w")
+        return inp
     
     def alert(self, title="", message=""):
         messagebox.showinfo(title, message)
--- src/views/FormView.py
+++ src/views/FormView.py
@@ -4,20 +4,23 @@ class FormView(sc):
     def __init__(self):
         super().__init__("Gerador de Relatório por data", "500x300")
         self.label("Data Início:", (0, 0))
-        self.data_inicio = self.dateInput((0, 1))
+        self.start_date = self.dateInput((0, 1))
         self.label("Data Fim:", (0, 2))
-        self.data_fim = self.dateInput((0, 3))
-        self.button("Enviar", self.generateReport, (1, 1), colspan=2)
+        self.end_date = self.dateInput((0, 3))
+        self.label("Formato de exportação:", (1, 1), colspan=2, sticky="e")
+        self.exportTo = self.combobox([".csv", ".json"], (1, 3))
+        self.button("Enviar", self.generateReport, (2, 1), colspan=2)
     
     def generateReport(self):
-        inicio = self.data_inicio.get_date()
-        fim = self.data_fim.get_date()
-        if inicio>fim:
+        start = self.start_date.get_date()
+        end = self.end_date.get_date()
+        if start>end:
             self.alert("Erro de inserção", "A data de Início deve ser inferior a Data Final para gerar relatóro.")
         print("-="*15)
         print("Relatório atual:")
-        print("Data de Início: ", self.data_inicio.get_date())
-        print("Data Final: ", self.data_fim.get_date())
+        print("Data de Início: ", self.start_date.get_date())
+        print("Data Final: ", self.end_date.get_date())
+        print("Formato do arquivo gerado: ", self.exportTo.get())
         print("-="*15)
 
 if __name__ =="__main__":

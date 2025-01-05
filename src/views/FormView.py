4b40cedba066d0dbc246b77e47a1b23ae2894d54
diogoJoseFreitas <diogoJoseFreitas@proton.me>
2025-01-05 01:07:51 -0400

    Cofigurado Data Início para um mês antes da data atual.

--- src/views/FormView.py
+++ src/views/FormView.py
@@ -1,15 +1,22 @@
 from views.templates.guiInterfaceTemplate import guiInterfaceTemplate as git
+from datetime import datetime, timedelta
 
 class FormView(git):
     def __init__(self):
         super().__init__("Gerador de Relatório por data", "500x300")
+
         self.label("Data Início:", position=(0, 0))
         self.start_date = self.dateInput(position=(0, 1))
+        self.start_date.set_date((datetime.today - timedelta(weeks=4)))
+
         self.label("Data Fim:", position=(0, 2))
         self.end_date = self.dateInput(position=(0, 3))
+
         self.label("Formato de exportação:", position=(1, 1), columnspan=2)
         self.exportTo = self.combobox([".csv", ".json"], position=(1, 3), width=5)
+
         self.button("Enviar", self.generateReport, position=(2, 1), columnspan=2, sticky="nsew")
+
         self.result = {}
     
     def getResult(self) -> dict:
@@ -18,6 +25,7 @@ class FormView(git):
     def generateReport(self):
         start = self.start_date.get_date()
         end = self.end_date.get_date()
+
         if start>end:
             self.alert("Erro de inserção", "A data de Início deve ser inferior a Data Final para gerar relatóro.")
         else:
@@ -26,6 +34,7 @@ class FormView(git):
                 "end_date": end,
                 "exportTo": self.exportTo.get()
             }
+            
         self.destroy()
 
 if __name__ =="__main__":
